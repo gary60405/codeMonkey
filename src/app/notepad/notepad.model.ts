2be38038efa1e248affeca70e23b3e06291df8e9
Gary <gary60405@gmail.com>
2018-01-27 11:16:19 +0800

    good

--- src/app/notepad/notepad.model.ts
+++ src/app/notepad/notepad.model.ts
@@ -2,68 +2,99 @@ import Blockly from 'node-blockly/browser';
 
 export class Toolbox {
   public xmlText = `<xml id="toolbox" style="display: inline;">
-                      <category name="Control">
-                        <block type="step"></block>
-                        <block type="turn"></block>
-                        <block type="controls_if"></block>
-                        <block type="controls_repeat_ext"></block>
-                        <block type="logic_compare"></block>
+                      <category name="猴子與烏龜">
+                        <block type="turtle_step"></block>
+                        <block type="turtle_turn"></block>
+                        <block type="monkey_step"></block>
+                        <block type="monkey_turn"></block>
+                      </category>
+                      <category name="老鼠抓火柴">
+                        <block type="turtle_step"></block>
+                        <block type="turtle_turn"></block>
+                        <block type="monkey_step"></block>
+                        <block type="monkey_turn"></block>
                         <block type="math_number"></block>
-                        <block type="math_arithmetic"></block>
-                        <block type="text"></block>
-                        <block type="text_print"></block>
+                        <block type="controls_repeat_ext"></block>
                       </category>
                     </xml>`;
-  public step = {
-    init:
-      function() {
-        this.appendDummyInput()
-            .appendField('step')
-            .appendField(new Blockly.FieldNumber(0, 0), 'stepValue');
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip('');
-        this.setHelpUrl('');
-      }
-  };
-  public turn = {
-    init:
-      function() {
-        this.appendDummyInput()
-            .appendField('turn')
-            .appendField(new Blockly.FieldAngle(0), 'angleValue');
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip('');
-        this.setHelpUrl('');
-      }
-  };
+
   public turtle_step = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField('烏龜走')
-          .appendField(new Blockly.FieldNumber(0, 0, 30), 'turtleStep')
-          .appendField('步');
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(230);
-   this.setTooltip('');
-   this.setHelpUrl('');
+    init:
+      function() {
+        this.appendDummyInput()
+            .appendField('烏龜走')
+            .appendField(new Blockly.FieldNumber(0, 0, 30), 'turtleStep')
+            .appendField('步');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+  };
+  public turtle_turn = {
+    init:
+      function() {
+        this.appendDummyInput()
+            .appendField('烏龜轉')
+            .appendField(new Blockly.FieldAngle(0), 'turtleTurn')
+            .appendField('度');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+  };
+  public monkey_step = {
+    init:
+      function() {
+        this.appendDummyInput()
+            .appendField('猴子走')
+            .appendField(new Blockly.FieldNumber(0, 0, 30), 'monkeyStep')
+            .appendField('步');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip('');
+        this.setHelpUrl('');
+    }
+  };
+  public monkey_turn = {
+    init:
+      function() {
+        this.appendDummyInput()
+            .appendField('猴子轉')
+            .appendField(new Blockly.FieldAngle(0), 'monkeyTurn')
+            .appendField('度');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip('');
+        this.setHelpUrl('');
     }
   };
 }
 
 export class GetCode {
-  public stepValue = (block) => {
+  public turtleStep = (block) => {
     const id = '1511104738945';
-    const value = block.getFieldValue('stepValue');
+    const value = block.getFieldValue('turtleStep');
     return `diveLinker.Send(${id}, ${value});`;
   }
-  public angleValue = (block) => {
+  public turtleTurn = (block) => {
     const id = '1511104858728';
-    const value = block.getFieldValue('angleValue');
+    const value = block.getFieldValue('turtleTurn');
+    return `diveLinker.Send(${id}, ${value});`;
+  }
+  public monkeyStep = (block) => {
+    const id = '1511104738945';
+    const value = block.getFieldValue('monkeyStep');
+    return `diveLinker.Send(${id}, ${value});`;
+  }
+  public monkeyTurn = (block) => {
+    const id = '1511104858728';
+    const value = block.getFieldValue('monkeyTurn');
     return `diveLinker.Send(${id}, ${value});`;
   }
 }
--- src/app/notepad/notepad.service.ts
+++ src/app/notepad/notepad.service.ts
@@ -16,14 +16,16 @@ export class NotepadService {
 init(toolBox) {
   Blockly.setLocale(zhHant);
   this.injectToWorkspace(toolBox.xmlText);
-  Blockly.Blocks['step'] = toolBox.step;
-  Blockly.Blocks['turn'] = toolBox.turn;
+  Blockly.Blocks['turtle_step'] = toolBox.turtle_step;
+  Blockly.Blocks['turtle_turn'] = toolBox.turtle_turn;
+  Blockly.Blocks['monkey_step'] = toolBox.monkey_step;
+  Blockly.Blocks['monkey_turn'] = toolBox.monkey_turn;
   // console.log(Blockly.Xml.blockToDom(this.workspace.newBlock('step')));
   // console.log(Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(this.workspace)));
     // tslint:disable-next-line:max-line-length
-    const text = '<xml xmlns="http://www.w3.org/1999/xhtml"><variables></variables><block type="step" id="8GeI6$)I!9^ed);,re|5" x="15" y="75"><field name="stepValue">2</field><next><block type="turn" id="P3`p,wT+@LWfgkvWS|:`"><field name="angleValue">330</field><next><block type="controls_repeat_ext" id="n34rv=!j]q,hZX-NiG*G"><value name="TIMES"><block type="math_number" id="lM;7!jDF5Mi}XthZ1xr@"><field name="NUM">3</field></block></value><statement name="DO"><block type="step" id="m~~;7N}uWUbOXn!;D(##"><field name="stepValue">1</field><next><block type="turn" id="Z%9aKcGfFi7(8c]ZRir1"><field name="angleValue">225</field></block></next></block></statement><next><block type="turn" id=")Dww23b*aYqwo.L*-kvX"><field name="angleValue">45</field></block></next></block></next></block></next></block></xml>';
-    const xml = Blockly.Xml.textToDom(text);
-    Blockly.Xml.appendDomToWorkspace(xml, this.workspace);
+    // const text = '<xml xmlns="http://www.w3.org/1999/xhtml"><variables></variables><block type="step" id="8GeI6$)I!9^ed);,re|5" x="15" y="75"><field name="stepValue">2</field><next><block type="turn" id="P3`p,wT+@LWfgkvWS|:`"><field name="angleValue">330</field><next><block type="controls_repeat_ext" id="n34rv=!j]q,hZX-NiG*G"><value name="TIMES"><block type="math_number" id="lM;7!jDF5Mi}XthZ1xr@"><field name="NUM">3</field></block></value><statement name="DO"><block type="step" id="m~~;7N}uWUbOXn!;D(##"><field name="stepValue">1</field><next><block type="turn" id="Z%9aKcGfFi7(8c]ZRir1"><field name="angleValue">225</field></block></next></block></statement><next><block type="turn" id=")Dww23b*aYqwo.L*-kvX"><field name="angleValue">45</field></block></next></block></next></block></next></block></xml>';
+    // const xml = Blockly.Xml.textToDom(text);
+    // Blockly.Xml.appendDomToWorkspace(xml, this.workspace);
 }
 
 injectToWorkspace(xml: string) {
@@ -46,8 +48,10 @@ injectToWorkspace(xml: string) {
   });
 }
 async getCode(value: any) {
-  Blockly.JavaScript['step'] = value.stepValue;
-  Blockly.JavaScript['turn'] = value.angleValue;
+  // Blockly.JavaScript['turtle_step'] = value.turtleStep;
+  // Blockly.JavaScript['turtle_turn'] = value.turtleTurn;
+  // Blockly.JavaScript['monkey_step'] = value.monkeyStep;
+  // Blockly.JavaScript['monkey_turn'] = value.monkeyTurn;
   // const code = Blockly.JavaScript.workspaceToCode(this.workspace).split(';').reverse();
   // let i = code.length;
   // while (i--) {
@@ -88,13 +92,23 @@ async DOMParser(dom: any, target: string) {
           }
         }
       }
-    } else if (dom.getAttribute('type') === 'step') {
+    } else if (dom.getAttribute('type') === 'turtle_step') {
       const id = '1511104738945';
       const value = dom.childNodes[0].innerHTML;
       // tslint:disable-next-line:no-eval
       eval(`diveLinker.Send(${id}, ${value});`);
-    } else if (dom.getAttribute('type') === 'turn') {
+    } else if (dom.getAttribute('type') === 'turtle_turn') {
       const id = '1511104858728';
+      const value = dom.childNodes[0].innerHTML;
+      // tslint:disable-next-line:no-eval
+      eval(`diveLinker.Send(${id}, ${value});`);
+    } else if (dom.getAttribute('type') === 'monkey_step') {
+      const id = '1511104362549';
+      const value = dom.childNodes[0].innerHTML;
+      // tslint:disable-next-line:no-eval
+      eval(`diveLinker.Send(${id}, ${value});`);
+    } else if (dom.getAttribute('type') === 'monkey_turn') {
+      const id = '1511104719544';
       const value = dom.childNodes[0].innerHTML;
       // tslint:disable-next-line:no-eval
       eval(`diveLinker.Send(${id}, ${value});`);
